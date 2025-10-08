83dd6a6a256b125c2db0637d8bcbd63b8460c978
estella <emoodycal@gmail.com>
2025-10-08 15:22:53 -0700

    lab08 and beginning of lab09

--- lab08/im_so_sorry.py
+++ lab08/im_so_sorry.py
@@ -4,10 +4,54 @@ def blocks(n):
     else:
         return blocks(n-1) + n
 
+# recursion runs a loop over and over again by calling the function within itself until a certain criteria is met
 
+
+def factorial(n):
+    if n <= 0:
+        return 1
+    else:
+        return factorial(n-1) * n
+
+# challenge
+
+
+def is_palindrome(s):
+    if len(s) <= 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    else:
+        return is_palindrome(s[1:-1])
+
+# challenge
+
+
+def sum_of_digits(n):
+    if n == 0:
+        return 0
+    else:
+        return sum_of_digits(n // 10) + (n % 10)
+
+
+# challenge
+def print_count_down(n):
+    if n <= 0:
+        print("BOOM")
+    else:
+        print(n)
+        print_count_down(n-1)
+
+
+# for recursion, there must be a way for the function to not recurse or repeat, so you need a way for it to end.
+print_count_down(10)
+print(sum_of_digits(415))
+print(sum_of_digits(1111))
+'''print(is_palindrome("hello"))
+print(is_palindrome("racecar"))
+print(is_palindrome("tacocat"))
 print(blocks(15))
 print(blocks(4))
-print(blocks(8))
-print(blocks(0))
-print(blocks(-1))
-print(blocks(1))
+print(factorial(4))
+print(len(str(factorial(52))))
+print(factorial(8))'''
---
+++ lab09/hanoi.py
@@ -0,0 +1,16 @@
+def move(n, source, destination, auxiliary):
+    if n == 0:
+        return
+    move(n - 1, source, auxiliary, destination)
+    print(f"Move disk {n} from {source} to {destination}")
+    move(n - 1, auxiliary, destination, source)
+
+
+def move(n, source, destination, auxiliary):
+    if n > 0:
+        move(n - 1, source, auxiliary, destination)
+        print(f"Move disk {n} from {source} to {destination}")
+        move(n - 1, auxiliary, destination, source)
+
+
+move(3, "A", "B", "C")
