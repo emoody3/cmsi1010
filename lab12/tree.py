24a755ee71b0b7c4805ecd5bb07fa624b8808d07
estella <emoodycal@gmail.com>
2025-10-28 12:56:31 -0700

    lab12 tree edits

--- lab12/tree.py
+++ lab12/tree.py
@@ -2,7 +2,25 @@ from math import radians
 from Tree.core import Tree
 from PIL import Image
 
-background_color = (200, 240, 250)
+background_color = (100, 140, 250)
+leaf_color = (220, 243, 255)
+trunk_width = 40
+base_trunk_color = (160, 35, 59)
+small_branch_color = (240, 225, 48)
+branch_gradient = (*base_trunk_color, *small_branch_color)
+
+trunk_length = 250
+first_branch_line = (0, 0, 0, -trunk_length)
+scales_and_angles = [(0.7, radians(50)), (0.7, radians(-30))]
+age = 11
+
+tree = Tree(pos=first_branch_line, branches=scales_and_angles)
+tree.grow(age)
+tree.move_in_rectangle()
+image = Image.new("RGB", tree.get_size(), background_color)
+tree.draw_on(image, branch_gradient, leaf_color, trunk_width)
+image.show()
+'''background_color = (200, 240, 250)
 leaf_color = (30, 160, 30)
 trunk_width = 30
 base_trunk_color = (150, 40, 0)
@@ -19,4 +37,4 @@ tree.grow(age)
 tree.move_in_rectangle()
 image = Image.new("RGB", tree.get_size(), background_color)
 tree.draw_on(image, branch_gradient, leaf_color, trunk_width)
-image.show()
+image.show()'''
